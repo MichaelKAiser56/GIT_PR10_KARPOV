dfd4580a205d8a3218970608a1c0d46d4c02030c
Карпов Максим <mkarpov648@gmail.com>
2025-11-18 19:39:13 +0300

    Добовлена информация о студенте, ФИО и номер группы

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine(" -------------- Информация о студенте --------------");
+Console.WriteLine($"Группа: ИС-23А");
+Console.WriteLine($"ФИО: Карпов Максим Владимирович");
 Console.WriteLine($"Текущая дата: {DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")}");
+Console.WriteLine("-----------------------------------------------------");
